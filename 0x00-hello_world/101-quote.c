3fa0a930dd55abbf54f13ddb01658dd13bde2081
Tesfa-B <Tesfa.B11@proton.me>
2023-09-07 23:47:18 +0000

    easy and simple

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,9 +5,12 @@
  * write - prints to stdo
  * @nbtye: param for write function
  */
-int write(int nbtye);
-int main(void)
-	{
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+#include <stdio.h> 
+ 
+int main()
+	{	
+	  static const char hello[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"; 
+	    fwrite(hello, sizeof(hello) - 1, 1, stdout);
+	   return 1; 
 	}
+
