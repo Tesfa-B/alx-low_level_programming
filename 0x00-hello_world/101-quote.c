30a7b6fbd1d257cec683bf0ca102910812862cea
Tesfa-B <Tesfa.B11@proton.me>
2023-09-07 23:38:34 +0000

    Add comment for param

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,12 @@
 /**
  * main - prints a quote
  * Return: if fail return 1
- */	
-int write(int filedes, const char *buf, unsigned int nbyte);
-int main( )
+ * write - prints to stdo
+ * @nbtye: param for write function
+ */
+int write(int nbtye);
+int main(void)
 	{
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19 \n", 55);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 	}
