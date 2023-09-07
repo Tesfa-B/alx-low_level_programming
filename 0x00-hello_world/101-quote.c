a208b5a6a9d1e92f8718c2a4793c992c35caced8
Tesfa-B <Tesfa.B11@proton.me>
2023-09-07 23:59:05 +0000

    Remove lines

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,12 @@
 #include <stdio.h>
 /**
- * main - prints a quote
+ * main - prints quote
  * Return: if fail return 1
- * write - prints to stdo
- * @nbtye: param for write function
+ * fwrite - print stdo
  */
-#include <stdio.h> 
- 
-int main()
-	{	
-	  static const char hello[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"; 
-	    fwrite(hello, sizeof(hello) - 1, 1, stdout);
-	   return 1; 
-	}
+int main(void)
+	{
+	const char hello[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	fwrite(, sizeof(h) - 1, 1, stdout);
+	return (1);
 
