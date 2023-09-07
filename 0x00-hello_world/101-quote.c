3560733f30d8e7a021fba4e20f93ad4c8ebd7436
Tesfa-B <Tesfa.B11@proton.me>
2023-09-07 23:06:09 +0000

    Add char array

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+/**
+ * main - prints a quote
+ * Return: if fail return 1
+ */
 int main(void)
 	{
-		and that piece of art is useful" Dora korpar, 2015-10-19 \n
-			return 1;
+	char q[] = "and that piece of art is useful\" Dora korpar, 2015-10-19";
+	FILE *file = fopen("myfile.txt", "W");
+	fwrite(q, 1, sizeof(q), file);
+	char buffer[sizeof(q)];
+	fread(buffer[sizeof(buffer), file);
+	return (1);
 	}
