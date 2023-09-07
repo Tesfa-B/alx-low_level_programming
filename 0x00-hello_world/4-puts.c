02e060a1eaea2a5f640e2415e1e698a19a5273c4
Tesfa-B <Tesfa.B11@proton.me>
2023-09-07 20:01:48 +0000

    Working with C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 int main(void)
 	{
-          stringputs("Programming is like building multilingual	puzzle")
- 	  return 0;
-
-
+	puts("Programming is like building a multilingual puzzle \n");
+	return (0); 
 	}
