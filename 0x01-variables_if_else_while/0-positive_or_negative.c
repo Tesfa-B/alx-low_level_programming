5341b9983a670f004bda9b1778db19efef014117
Tesfa-B <Tesfa.B11@proton.me>
2023-09-08 19:53:51 +0000

    Rename the file correctely

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,6 +4,10 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+/**
+ * main - randomly checks number whether it is postive or negative ,zero
+ * Return: If success return 0
+ */
 int main(void)
 {
 	int n;
@@ -11,17 +15,17 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-        if (n > 0)
+	if (n > 0)
 	{
-	  printf(" %d is postive\n", n);
+		printf(" %d is postive\n", n);
 	}
 	else if (n == 0)
 	{
-	  printf("%d is zero\n", n);
+		printf("%d is zero\n", n);
 	}
 	else
 	{
-	  printf("%d is negative\n", n);
+		 printf("%d is negative\n", n);
 	}
 	return (0);
 }
