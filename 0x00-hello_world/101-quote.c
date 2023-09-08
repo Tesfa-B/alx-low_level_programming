41ec281cef9c7a64b7ff278d7c3b7d9ec85da55e
Tesfa-B <Tesfa.B11@proton.me>
2023-09-08 00:24:28 +0000

    Add blank line before and after declaration

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,9 @@
  */
 int main(void)
 	{
-	const char hello[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-	fwrite(, sizeof(h) - 1, 1, stdout);
-	return (1);
+	char h[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
+	fwrite(h, sizeof(h) - 1, 1, stdout);
+
+	return (1);
+	}
