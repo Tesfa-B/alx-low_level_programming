86ca1d103e23bfaacba1b4d7f7f1a6d5364b8490
Tesfa-B <Tesfa.B11@proton.me>
2023-09-09 16:54:13 +0000

    Add two loops for upper and lower

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
 /**
- * main - prints alphabet in lower case
- * Return: if success return 0;
+ * main - prints alphabet in lower case and upper case
+ * Return:if success return 0
  */
-int main (void)
+int main(void)
 {
- int alphabet:
- alphabet = "lower case";
-	putchar("%c", alphabet)
- 	return (0);
-{
+	char q;
+	char r = '\n';
+
+	for (q = 'a'; q <= 'z'; q++)
+		putchar(q);
+	for (q = 'A'; q <= 'Z'; q++)
+		putchar(q);
+		putchar(r);
+	return (0);
+}
