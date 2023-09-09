5b15551f81821b46579a6c386439f2acc48ce11d
Tesfa-B <Tesfa.B11@proton.me>
2023-09-09 19:07:20 +0000

    Add space and coma for base 10

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
 /**
- * main - prints single digits separted by ,
- * Return - if success return 0
+ * main - prints single digits separted by , and space
+ * Return: if success return 0
  */
 int main(void)
 {
-	int digit;
-	digit = "digit";
-        putchar("%d", digit);
+	int i;
+	int r = '\n';
+	int s = ' ';
+	int c = ',';
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+		putchar(c);
+		putchar(s);
+	}
+	putchar(r);
 	return (0);
 }
