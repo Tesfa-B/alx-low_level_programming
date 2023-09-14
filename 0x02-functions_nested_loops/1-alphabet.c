18956f7db2919e5d16b5bc550a7b74fce2809d91
Tesfa-B <Tesfa.B11@proton.me>
2023-09-14 10:18:49 +0000

    Aphabet in lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,11 @@
 #include "main.h"
-#include <stdio.h>
-
+/**
+ * main - calls function print_alphabet
+ * Return: if success print 0
+ */
 int main(void)
 {
-	int x;
-	for (x = 'a'; x <= 'z'; x++)
-	{
-		 _putchar(x);
-	 print_alphabet();	 
-	}
+	print_alphabet();
+
 	return (0);
 }
