e2f563b1f3debb744ca181691065d0074bea227e
Tesfa-B <Tesfa.B11@proton.me>
2023-09-14 12:16:05 +0000

    Print 10X alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints alphabet in lower case 10x
+ */
+void print_alphabet_x10(void)
+{
+	int x;
+	int w;
+
+	for (x = 0; x <= 10; x++)
+	{
+		for (w = 'a'; w <= 'z'; w++)
+		{
+			_putchar(w);
+		}
+		_putchar('\n');
+	}
+}
