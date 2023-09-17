5f2a7a0007861de7296b404034645c5a6cacad72
Tesfa-B <Tesfa.B11@proton.me>
2023-09-17 17:28:31 +0000

    Prints natural numbers

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_to_98 - prints natural numbres starting form the given number to 98
+ * @n: the number that printing  starts from
+ */
+void print_to_98(int n)
+{
+	while (n <= 98)
+	{
+		printf("%d, ", n);
+	n++;
+	}
+	while (n > 98)
+	{
+		printf("%d, ", n);
+	n--;
+	}
+	printf("\n");
+}
