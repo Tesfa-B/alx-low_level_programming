d7278e502c7b8cca9a748cf928670a5c9f55adab
Tesfa-B <Tesfa.B11@proton.me>
2023-09-17 11:34:21 +0000

    Add function that negates the value of negative

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - returns the absolute value of the number starting from zero
+ * @i: the number to be computed
+ * Return: -i if the number is negative else i
+ */
+int _abs(int i)
+{
+	if (i < 0)
+		return (-i);
+
+	return (i);
+
+}
