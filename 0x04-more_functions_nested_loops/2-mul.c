e6244182affd9e2f091e29ae6c3fd9ff3d0dfbed
Tesfa-B <Tesfa.B11@proton.me>
2023-09-21 06:51:29 +0000

    Add mul function

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - multiples two integer
+ * @a: the value to be multiplied
+ * @b: the value to be multiplied
+ * Return: return the value of c
+ */
+int mul(int a, int b)
+{
+	int c = a * b;
+
+	return (c);
+}
