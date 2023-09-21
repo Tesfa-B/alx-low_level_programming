3ba2536d2be3def5b69bcec8f1b6a825dbf75268
Tesfa-B <Tesfa.B11@proton.me>
2023-09-21 07:49:12 +0000

    Add print most numbers function

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints numbers except 2 and 4
+ */
+void print_most_numbers(void)
+{
+	int a = 48;
+
+	while (a < 58)
+	{
+		if (a != 50 && a != 52)
+		{
+			_putchar(a);
+		}
+	a++;
+	}
+	_putchar('\n');
+}
