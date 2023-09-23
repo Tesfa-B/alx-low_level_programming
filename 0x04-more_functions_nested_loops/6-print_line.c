9d3dd50897eda25136ef93344a76b031c8fcc24f
Tesfa-B <Tesfa.B11@proton.me>
2023-09-23 08:13:27 +0000

    Fix style

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,12 +2,14 @@
 /**
  * print_line - prints underscore n numbers of time
  * @n: the number _ to be printed.
+ * Return: return 0 if success
  */
 void print_line(int n)
 {
 	int i;
 
 	for (i = 0; i < n; i++)
-		_putchar(95);	
+		_putchar(95);
 	_putchar('\n');
+	return (0);
 }
