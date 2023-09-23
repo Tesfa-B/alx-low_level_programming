8dde761b66d9c8c62294884568d13d1a044c607f
Tesfa-B <Tesfa.B11@proton.me>
2023-09-23 08:16:56 +0000

    Remove  return (0)

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,7 +2,6 @@
 /**
  * print_line - prints underscore n numbers of time
  * @n: the number _ to be printed.
- * Return: return 0 if success
  */
 void print_line(int n)
 {
@@ -11,5 +10,4 @@ void print_line(int n)
 	for (i = 0; i < n; i++)
 		_putchar(95);
 	_putchar('\n');
-	return (0);
 }
