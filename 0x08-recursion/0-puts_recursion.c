77fe7272e5b60b33bde8b52b030a0838a232fa9a
Tesfa-B <Tesfa.B11@proton.me>
2023-09-29 13:51:18 +0000

    Add _strlen to find length

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,8 +1,26 @@
 #include "main.h"
 
+int _strlen(char *s);
+
 void _puts_recursion(char *s)
 {
-	_ptchar(*s + 0);
-	_putchara('\n');
-
+	int i = 0;
+	char ln = _strlen(s);
+       
+	for (i = s[0]; i < ln - 1; s++)
+	{
+		_putchar(s[0] + 0);
+	}
+	_putchar('\n');
 }
+int _strlen(char *s)
+{	
+	unsigned int len = 0;
+
+	while (s[len] != '\0')
+	{
+		s++;
+	}
+	return (s);
+}
+
