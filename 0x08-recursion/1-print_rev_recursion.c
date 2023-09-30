ff11966618f82eb9f176aaac6f308635ea3195d4
Tesfa-B <Tesfa.B11@proton.me>
2023-09-30 15:57:35 +0000

    Add reverse recursive

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,13 +1,15 @@
 #include "main.h"
+/**
+ * _print_rev_recursion - prints the given valie in reverse
+ * @s: to defrenece value of *s by adding 1
+ */
 
 void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		_putchar('\n');
 		return;
 	}
-	_putchar(*s + 0);
-	s--;
-	_print_rev_recursion(s);	
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
 }
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,6 @@
+#include "main.h"
+
+int is _prime_number(int n)
+{
+	
+}
