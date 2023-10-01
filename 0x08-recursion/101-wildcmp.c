ba00702753c327dee442f9af198685c0ee1fdeca
Tesfa-B <Tesfa.B11@proton.me>
2023-10-01 22:21:38 +0000

    Add compare recursion

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,6 +1,29 @@
-#include "main.h'
+#include "main.h"
+/**
+ * wilscmp - compare the strings
+ * @s1: pointer for the string 1
+ * @s2: pointer for string 2
+ * Return: return 0
+ */
 
 int wildcmp(char *s1, char *s2);
 {
+	if (*s1 == '\0')
+	{
+		if (*s2 != '\0' && *s2 == '*')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		return (*s2 == '\0');
+	}
 
+	if (*s2 == '*')
+	{
+		return (wildcmp(s1 + 1, s2) || wilscmp(s1, s2 + 1));
+	}
+	else if (*s1 == *s2)
+	{
+		return (wilscmp(s1 + 1, s2 + 1));
+	}
+	return (0);
 }
