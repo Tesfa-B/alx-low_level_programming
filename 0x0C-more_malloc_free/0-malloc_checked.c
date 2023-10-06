0cfe9d0946757c0a9273b99a3f78d78ae38e0110
Tesfa-B <Tesfa.B11@proton.me>
2023-10-06 10:53:00 -0700

    Fix style

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,12 +1,16 @@
 #include "main.h"
-
+/**
+ * *malloc_checked - daynamically allocate memory
+ * @b: the passed value
+ * Return:  98 if NULL
+ */
 void *malloc_checked(unsigned int b)
 {
 	unsigned int *assign = malloc(sizeof(unsigned int));
 	*assign = b;
 
 	if (assign == NULL)
-		return (void *)98;
+		return ((void *) 98);
 	return (assign);
 
 }
