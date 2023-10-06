28c33cd96310781ae0e7f0e79bcdeff54e208fdf
Tesfa-B <Tesfa.B11@proton.me>
2023-10-06 11:54:26 -0700

    Add array

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,15 +1,19 @@
 #include "main.h"
-
+/**
+ * *array_range - allocates the values in memory
+ * @min: the first value
+ * @max:the last value
+ * Return: if min > max return NULL else arr 
+ */
 int *array_range(int min, int max)
 {
 	int *arr = malloc(sizeof(int));
-	int i;
 
-	for (i = 0; i <= max; i++)
-		arr[i] = min; 
+	arr[0] = min;  
 	if (min > max)
 		return (NULL);
 	if (arr == 0)
 		return (NULL);
 	return (arr);
+	free(arr);
 }
