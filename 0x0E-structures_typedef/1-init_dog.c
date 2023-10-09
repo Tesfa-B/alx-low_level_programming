75b8abca25b0b9a5a86e7d677b53c2135e79d488
Tesfa-B <Tesfa.B11@proton.me>
2023-10-09 21:22:37 +0000

    Add struct Dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,5 @@
+#include "dog.h"
+
+void int_dog(struct dog *d, char *name, float age, char *owner)
+{
+
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - Details for  Dog
+ * @name: name fot the dog
+ * @age:  name for the dog
+ * @owner: owner of dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} my_dog;
+#endif
