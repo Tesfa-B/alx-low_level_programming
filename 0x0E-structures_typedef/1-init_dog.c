c1561432079152f04b3d04a1e5c2d7158cc715ef
Tesfa-B <Tesfa.B11@proton.me>
2023-10-10 15:34:41 +0000

    Print struct dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,9 @@
 #include "dog.h"
 
-void int_dog(struct dog *d, char *name, float age, char *owner)
-{
+void init_dog(struct dog *d, char *name, float age, char *owner)
 
+struct dog *d
+{
+	.char *name;
+
+}d;
---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,22 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * print_dog - print the information of the dog
+ * @struct dog -  a structure that stores information about dog
+ * @d: a of dog structure
+ *
+ */
+void print_dog(struct dog *d)
+{
+	if (d->name == 0 && d->age == 0)
+	{
+		printf("(nil)");
+	}
+	if (*d->name == '\0')
+	{
+		printf("Name: (nil)");
+	}
+	printf("Name: %s\n", d->name);
+	printf("Age : %f\n", d->age);
+	printf("Owner : %s\n", d->owner);
+}
