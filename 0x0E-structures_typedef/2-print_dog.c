b6e58e38fd287f62fd672258fab818f74841af66
Tesfa-B <Tesfa.B11@proton.me>
2023-10-10 20:19:14 +0000

    Remove printf

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -18,7 +18,5 @@ void print_dog(struct dog *d)
 	{
 		d->owner = "(nil)";
 	}
-	printf("Name: %s \n", d->name);
-	printf("Age: %f \n", d->age);
-	printf("Owner: %s \n", d->owner);
+	printf("Name: %s\n Age: %f\n Owner: %s\n", d->name, d->age, d->owner);
 }
