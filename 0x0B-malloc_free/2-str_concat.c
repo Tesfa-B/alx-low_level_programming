a140adab57c6cd73b12bcfab758618c81cba844f
Tesfa-B <Tesfa.B11@proton.me>
2023-10-10 20:27:40 +0000

    Concat string

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,7 +2,7 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	int *concat;
+	char *concat;
 
 	if (s1 || s2 == '\0')
 	{
@@ -10,7 +10,7 @@ char *str_concat(char *s1, char *s2)
 	}
 	else
 	{
-		concat = malloc(sizeof (int));
+		concat = malloc(sizeof (char));
        		concat = s1 + s2;
 	}
 	return (0);
