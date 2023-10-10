e0a78a32d7a119b8d6f4eb9f74cb5d23a71eadbc
Tesfa-B <Tesfa.B11@proton.me>
2023-10-10 20:24:30 +0000

    Remove curly braces

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,12 +11,8 @@ void print_dog(struct dog *d)
 		return;
 
 	if (d->name == NULL)
-	{
 		d->name = "(nil)";
-	}
 	if (d->owner == NULL)
-	{
 		d->owner = "(nil)";
-	}
 	printf("Name: %s\n Age: %f\n Owner: %s\n", d->name, d->age, d->owner);
 }
