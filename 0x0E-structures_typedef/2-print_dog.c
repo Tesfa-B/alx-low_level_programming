0026aed7fb9dd2f83569e47714eac41cf4d422ba
Tesfa-B <Tesfa.B11@proton.me>
2023-10-10 20:11:59 +0000

    Fix again

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -7,14 +7,14 @@
  */
 void print_dog(struct dog *d)
 {
-	if (d == NULL){
+	if (d == NULL)
 		return;
 
-	if (d->name == '\0')
+	if (d->name == NULL)
 	{
 		d->name = "(nil)";
 	}
-	if (d->owner == '\0')
+	if (d->owner == NULL)
 	{
 		d->owner = "(nil)";
 	}
