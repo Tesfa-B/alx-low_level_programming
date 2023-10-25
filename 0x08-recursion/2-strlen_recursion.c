caf40a10a0d5313f2442df182332be18cade1bdf
Tesfa-B <Tesfa.B11@proton.me>
2023-10-25 09:53:39 -0700

    Recursion for length

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,11 +1,17 @@
 #include "main.h"
-
-
+/**
+ * _strlen_recursion - calculates length of the value
+ * @s: the value to be calculated
+ * Return: lengthe of the given value
+ */
 int _strlen_recursion(char *s)
 {
-	if (*s == '\0')
+	int j = 0;
+
+	while (*s != '\0')
 	{
-		return (1);
+		++j;
+		_strlen_recursion(++s);
 	}
-	return _strlen_recursion(++s);
+	return (j);
 }
