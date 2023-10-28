ad8b209a1781e1a20968a2247e6f7747bd3c9ef7
Tesfa-B <Tesfa.B11@proton.me>
2023-10-28 18:52:09 +0000

    Add equals to 0

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -4,29 +4,26 @@
  * @argc: number of arguments
  * @argv: array of values
  * atoi - changes string to intger
- * Return: if success 0 else i1
+ * Return: if success 0 else 1
  */
 int main(int argc, char *argv[])
 {
 	int j, add = 0;
 
-	if (argc < 1)
+	if (argc <= 1)
 	{
 		printf("0\n");
 	}
 	for (j = 1; j < argc; j++)
 	{
 
-		if (*argv[j] >= 48 && *argv[j] <= 57)
-		{
-			add = add + atoi(argv[j]);
-		}
-		else
+		if (*argv[j] < 48 && *argv[j] > 57)
 		{
 			printf("Error\n");
 			return (1);
 		}
+		add = add + atoi(argv[j]);
+		printf("%d\n", add);
 	}
-	printf("%d\n", add);
 	return (0);
 }
