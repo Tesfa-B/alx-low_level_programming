2e1138abc8e60c1fc045ad059caf94feebd9a661
Tesfa-B <Tesfa.B11@proton.me>
2023-10-28 18:29:40 +0000

    Add nums

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,57 +1,32 @@
-nclude <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
-	/**
-	 * 	 * check_num - check - string there are digit
-	 * 	 	 * @str: array str
-	 * 	 	 	 * Return: Always 0 (Success)
-	 * 	 	 	 	 */
-	int check_num(char *str)
-		{
-					/*Declaring variables*/
-					unsigned int count;
-							
-							count = 0;
-									while (count < strlen(str)) /*count string*/
-												{
-																if (!isdigit(str[count])) /*check if str there are digit*/
-																				{
-																									return (0);
-																												}
-																		   count++;
-																		   		}
-											return (1);
-												}
-	/**
-	 * 	 * main - Print the name of the program
-	 * 	 	 * @argc: Count arguments
-	 * 	 	 	 * @argv: Arguments
-	 * 	 	 	 	 * Return: Always 0 (Success)
-	 * 	 	 	 	 	 */
-	int main(int argc, char *argv[])
-		{
-					/*Declaring variables*/
-					int count;
-							int str_to_int;
-									int sum = 0;
-											count = 1;
-													while (count < argc) /*Goes through the whole array*/
-																{
-																				if (check_num(argv[count]))
-																								{
-																													str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
-																																	sum += str_to_int;
-																																				}
-																							/*Condition if one of the number contains symbols that are not digits*/
-																							else
-																											{
-																																printf("Error\n");
-																																				return (1);
-																																							}
-																										count++;
-																												}
-															printf("%d\n", sum); /*print sum*/
-																	return (0);
-																		}
+#include "main.h"
+/**
+ * main - prints the sum of the given numbers
+ * @argc: number of arguments
+ * @argv: array of values
+ * atoi - changes string to intger
+ * Return: if success 0 else i1
+ */
+int main(int argc, char *argv[])
+{
+	int j, add = 0;
 
+	if (argc < 1)
+	{
+		printf("0\n");
+	}
+	for (j = 1; j < argc; j++)
+	{
+
+		if (*argv[j] >= 48 && *argv[j] <= 57)
+		{
+			add = add + atoi(argv[j]);
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	printf("%d\n", add);
+	return (0);
+}
