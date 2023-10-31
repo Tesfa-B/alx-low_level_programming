63a4ec3f7e4334c62322808c1019507d61057516
Tesfa-B <Tesfa.B11@proton.me>
2023-10-31 15:45:55 +0000

    Add malloc function

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * create_array - creates array by daynamically allocating
+ * @size: size of the arary
+ * @c: the value of the array
+ * Return: if success the the value else 1;
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *j;
+	unsigned int b;
+
+	j = (char *) malloc(sizeof(c) * size);
+
+	if (size == 0)
+		return (NULL);
+	for (b = 0; b <= size; b++)
+	{
+		j[b] = c;
+	}
+	return (j);
+	free(j);
+}
