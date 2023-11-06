325e395f62314b36e8fed12ddf6ffb1fb82057a7
Tesfa-B <Tesfa.B11@proton.me>
2023-11-06 19:39:34 +0000

    Add again

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -74,7 +74,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 	{
 	free(dog);
 		return (NULL);
-	}																		dog->owner = malloc(sizeof(char) * (len2 + 1))
+	}
+	dog->owner = malloc(sizeof(char) * (len2 + 1));
 	if (dog->owner == NULL)
 	{																		free(dog);
 	free(dog->name);
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,8 +14,13 @@ typedef struct dog
 	float age;
 	char *owner;
 } dog_t;
+/**
+ * dog_t - typdef for struct dog
+ */
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-void free_dog(dog_t *d);
-
+void free_dog(dog_t *d)
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 #endif
