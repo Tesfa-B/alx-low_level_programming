0d1475bee1b0b095fbfb0d41e83a98c4f46c7fc1
Tesfa-B <Tesfa.B11@proton.me>
2023-11-06 16:13:27 +0000

    Add File macro

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,24 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int run(int argc, int argv[])
-{
-	int i;
-
-	if (argc > 0)
-	{
-		return (1);
-	}
-	for(i = 0; argv[i] > 0; i++)
-	{
-		printf("%i\n", argv[i]);
-	}
-	return (0);
-}
 int main()
-{	
-	int i;
-
-	i = run();
-	return (x);
+{
+	printf("%s\n", __FILE__);	
+	return (0);
 }
