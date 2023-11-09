f035640ea1e781a1ecf64fcb328d016dfba33048
Tesfa-B <Tesfa.B11@proton.me>
2023-11-09 18:30:22 +0000

    Add print numberes

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -3,7 +3,7 @@
 /**
  * print_numbers - prints all numbers
  * @separator: is the placed after the number
- * @n: number of argument 
+ * @n: number of argument
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
@@ -11,16 +11,17 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list aj;
 	int m;
-	
+
 	va_start(aj, n);
-	if (x == 0)
-		return;
 	if (n != 0)
 		for (i = 0; i < n; i++)
 		{
 			m = va_arg(aj, int);
-			printf("%d %c", m,x);
-
+			printf("%d", m);
+			if (x == '\0')
+				return;
+			if (i < n - 1)
+			printf("%c ", x);
 		}
 		va_end(aj);
 		putchar('\n');
