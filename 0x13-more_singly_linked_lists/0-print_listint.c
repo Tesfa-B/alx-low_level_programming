3c7ef43cc3668340adbec307fd0e8a3a394b54a2
Tesfa-B <Tesfa.B11@proton.me>
2023-11-21 09:36:35 +0000

    Print node list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+#include <stddef.h>
+#include <stdio.h>
+
+
+size_t print_listint(const listint_t *h)
+{
+	if (h->n == 0)
+	{
+		return(1);
+	}
+	printf("%d\n%d\n", h->n, h->next->n);
+	
+	return (0);
+}
