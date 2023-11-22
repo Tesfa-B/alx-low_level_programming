4bd9f1c3ce81b9d35c676b200f0fda5a5e3d68c1
Tesfa-B <Tesfa.B11@proton.me>
2023-11-22 18:49:59 +0000

    Add sum for node

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,15 +1,21 @@
 #include "lists.h"
-
+/**
+ * sum_listint - adds the value of the nodes
+ * @head: the first node
+ * Return: return the sum of the nodes
+ */
 int sum_listint(listint_t *head)
 {
 	listint_t *temp;
 	int sum = 0;
 
+	if (head == NULL)
+		return (0);
 	temp = head;
 	while (temp->next != NULL)
 	{
-		sum += temp->n
+		temp = temp->next;
+		sum += temp->n;
 	}
 	return (sum);
 }
-	
