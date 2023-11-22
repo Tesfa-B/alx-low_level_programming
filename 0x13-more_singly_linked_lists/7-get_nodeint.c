69c6d7eefa9e14dd103dff7fc45a2eed7389968e
Tesfa-B <Tesfa.B11@proton.me>
2023-11-22 16:22:39 +0000

    Add get node by index

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,14 +1,22 @@
 #include "lists.h"
-
+/**
+ * get_nodeint_at_index - get nodes by given index
+ * @index: given number of index for node
+ * @head: the first node
+ * Return: returns the node
+ */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	listint_t *temp;
-	int x;
+	unsigned int i;
+
+	if (head == NULL)
+		return (0);
 
 	for (i = 0; i < index; i++)
 	{
+		temp = head;
 		temp = temp->next;
-		return (temp);
 	}
-	return (0);
+	return (temp);
 }
