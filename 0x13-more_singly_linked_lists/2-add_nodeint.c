bbebe245d377bb07af2d203caeb373b09af7bfdb
Tesfa-B <Tesfa.B11@proton.me>
2023-11-22 13:03:04 +0000

    Add if for the NULL

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -10,8 +10,9 @@ listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *firstnode;
 
+	if (head == NULL)
+		return (NULL);
 	firstnode = malloc(sizeof(listint_t));
-
 	if (firstnode == NULL)
 		return (NULL);
 
