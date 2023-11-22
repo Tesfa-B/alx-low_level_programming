3b1a30612e5b2e3d7fad115f5fc99826cdaf6968
Tesfa-B <Tesfa.B11@proton.me>
2023-11-22 19:00:26 +0000

    Add free all node

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,11 +9,11 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
+	temp = *head;
 	while (head != NULL)
 	{
-		temp = *head;
 		temp = temp->next;
 		free(temp);
-		*head = NULL;
 	}
+	head = NULL;
 }
