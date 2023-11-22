4b3033e7942d4af0b483adeedc199ef650e17529
Tesfa-B <Tesfa.B11@proton.me>
2023-11-22 14:19:18 +0000

    Add if NULL

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,10 +1,16 @@
 #include "lists.h"
 #include <stdlib.h>
-
+/**
+ * free_listint - frees the node from memory
+ * @head: head node
+ * Return: return if there is nothing
+ */
 void free_listint(listint_t *head)
 {
 	listint_t *temp;
-	
+
+	if (head == NULL)
+		return;
 	temp = head;
 	head = head->next;
 	free(temp);
