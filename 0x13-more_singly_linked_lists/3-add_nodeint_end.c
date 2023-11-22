dcce8899ca0711872428d81721ef7d9687e6f300
Tesfa-B <Tesfa.B11@proton.me>
2023-11-22 14:09:28 +0000

    Fix logic

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -19,16 +19,13 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 
 	if (*head == NULL)
 		*head = lastnode;
-	else
-	{
-		temp = *head;
-	}
+	temp = *head;
 	while (temp->next != NULL)
 	{
 		temp = temp->next;
 	}
-	lastnode->next = NULL;
 	temp->next = lastnode;
+	lastnode->next = NULL;
 
 	return (lastnode);
 }
