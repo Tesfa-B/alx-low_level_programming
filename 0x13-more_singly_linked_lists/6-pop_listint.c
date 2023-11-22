00bdd75334ece04b90e835fb3f303ba02451d105
Tesfa-B <Tesfa.B11@proton.me>
2023-11-22 16:02:41 +0000

    Add comments

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,12 +1,16 @@
 #include "lists.h"
 #include <stdlib.h>
-
+/**
+ * pop_listint - pops the first node send returns data
+ * @head: the first node
+ * Return: the data of the first node
+ */
 int pop_listint(listint_t **head)
 {
 	listint_t *temp;
 	int value;
 
-	if(*head == NULL)
+	if (*head == NULL)
 		return (0);
 	temp = *head;
 	*head = temp->next;
