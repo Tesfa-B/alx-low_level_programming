a601b7754afcf6f049c7f6197d12c583c74b90d1
Tesfa-B <Tesfa.B11@proton.me>
2023-12-22 07:08:50 +0000

    Print in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,16 +1,33 @@
 #include "main.h"
-
+/**
+ * print_rev - prints string in reverse
+ * @s: the string to be reversed
+ */
 void print_rev(char *s)
 {
-	int i;
+	int h  = _strlen(s);
 
- 	for (i = 0; *s != '\0'; i++)
+	while (h != 0)
 	{
-		s++;
-		if (*s == '\0')
-		{
-			--s;
-		}
+		h--;
+		_putchar(s[h]);
 	}
 	_putchar('\n');
+}
+/**
+ * _strlen - calculates string length
+ * @s: string
+ * Return: retuns string length
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*s != '\0')
+	{
+		i++;
+		s++;
+	}
+	return (i);
+
 }
--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,7 +1,25 @@
 #include "main.h"
 
-void rev_string(char *s);
+void rev_string(char *s)
 {
-	
-}
+       	int h  = _strlen(s) - 1;
 
+        while ( *s != '\0')
+        {
+                _putchar(s[h]);
+                s--;
+        }
+        _putchar('\n');
+}
+int _strlen(char *s)
+{
+        int i = 0;
+
+        while (i != '\0')
+        {
+                i++;
+                s++;
+        }
+        return (i);
+
+}	
